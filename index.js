6feb76016d8cd529964263eec0c39e37b66a27cc
vinithkumar6 <49217348+vinithkumar6@users.noreply.github.com>
2023-10-17 22:07:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const db = require('./config/mongoose');
+const session = require('express-session');
+const passport = require('passport');
+const passportLocal = require('./config/passport-local-startegy');
+
+dotenv.config({ path: 'config/.env' });
+
+const app = express();
+
+// set ejs as view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 1000 * 60 * 100 },
+  })
+);
+
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+app.use(express.urlencoded({ extended: true }));
+
+// for authentication
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(passport.setAuthenticatedUser);
+
+// express router
+app.use('/', require('./routes'));
+
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+
+
+// listen on port
+app.listen(port, function (error) {
+  if (error) {
+    console.log(`Error in connecting to server: ${error}`);
+    return;
+  }
+  console.log(`Server running on port: ${port}`);
+});
